82a5f9d05ecd658a823e42f157e70cf752a13101
stevekutz <stkutz@gmail.com>
2020-04-27 22:24:22 -0500

    defined template, a simple view, & setup database

--- todo/tasks/admin.py
+++ todo/tasks/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import *
+#from tasks.models import Task  #will also work
+# .models import Task      #will also work
+admin.site.register(Task)
