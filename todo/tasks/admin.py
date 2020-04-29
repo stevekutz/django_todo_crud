d36e18d1c2bee7a24cb42a556fcfff643c5e25e1
stevekutz <stkutz@gmail.com>
2020-04-28 21:42:12 -0500

    created form, updated view & template to allow new todos to be added

--- todo/tasks/admin.py
+++ todo/tasks/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import *
-#from tasks.models import Task  #will also work
-# .models import Task      #will also work
+#from tasks.models import Task  # will also work
+# .models import Task      # will also work
 admin.site.register(Task)
--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,11 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from .models import *
+from .forms import *
 
 # Create your views here.
 def index(request):
     tasks = Task.objects.all()  # returns everything, e.g.  returns a new QuerySet that is a copy of the current one
 
-    context = {'tasks': tasks}  # create a context dictionary
+    form = TaskForm() 
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+
+        if form.is_valid:
+            form.save()
+        return  redirect('/')   
+    
+    context = {'tasks': tasks, 'form': form}  # create a context dictionary
     return render(request, 'tasks/list.html', context)
